bacf06ca3fbd7a52a190d996324552bda74fa32b
JerusalemProgramming.com <jerusalemprogramming@gmail.com>
2018-12-24 11:02:52 +0200

    Create program_CalculateExponentialPowersPrintListComprehensions.py

---
+++ program_CalculateExponentialPowersPrintListComprehensions.py
@@ -0,0 +1,48 @@
+## CODE BELOW IN PYTHON
+
+## INSTRUCTIONS:
+## Write a program using for loops (in Java, JavaScript, C, or Python) that prints out:
+## 1.) the numbers 1 through 10 on the first line,
+## 2.) the squares of 1 through 10 on the second line (e.g. 1, 4, 9...),
+## 3.) the cubes on line three (e.g. 1, 8, 27...),
+## ...and so on, for ten lines.
+## Please be sure to indicate which language you are using.
+
+## BEGIN MAIN PROGRAM
+## BEGIN MAIN PROGRAM
+## BEGIN MAIN PROGRAM
+
+## IMPORT MODULES (NONE NECESSARY)
+
+## DECLARE VARIABLES
+count = 1
+
+ListOfIntegers = list(range(1,11,1))  ## range(start, stop, step)
+
+## CALCULATIONS
+for each in ListOfIntegers:
+   
+    ListNew = [each**count for each in ListOfIntegers] ## PYTHON LIST COMPREHENSION
+
+    print(ListNew, "count = ", count) ## PRINT OUTPUT - TESTING PUPOSES
+    
+    count = count + 1 ## LAST STEP
+
+
+## END MAIN PROGRAM
+## END MAIN PROGRAM
+## END MAIN PROGRAM
+
+## GAME OVER
+    
+## WE HOPE YOU ENJOYED, AND THAT THIS HELPS YOUR UNDERSTANDING OF USING PYTHON LANGUAGE TO SOLVE PROBLEMS WITH PYTHON PROGRAMMING
+## PLEASE COME BACK AGAIN SOON
+## PLEASE VISIT OUR WEB SITES (OUR PROBLEM-SOLVING PROGRAMMING & DEVELOPMENT SERVICES ARE AVAILABLE FOR HIRE):
+## www.JerusalemProgrammer.com
+## www.JerusalemProgrammers.com
+## www.JerusalemProgramming.com
+
+    
+
+    
+
